034ff4ea70b206b66813a9119e6710cfa44b524a
deloabass <delomodibo@gmail.com>
2025-07-07 14:26:26 +0100

    b

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -104,7 +104,7 @@ const Footer = () => {
         </div>
 
         <div className="flex items-center justify-between pt-10 mb-8">
-          <a href="/" className="reveal-up">
+          <a href="abassa.onrender.com" className="reveal-up">
             <img src="/images/logo.png" width={40} height={40} alt="Logo" />
           </a>
           <p className="text-zinc-500 tet-sm reveal-up">
